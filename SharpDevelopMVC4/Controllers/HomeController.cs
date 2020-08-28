fde264ded5d6c4a6d32ed861292177534ea64485
Bernard Gabon <hewbertgabon@gmail.com>
2020-08-29 01:51:05 +0800

    Added Dapper

--- SharpDevelopMVC4/Controllers/HomeController.cs
+++ SharpDevelopMVC4/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.OleDb;
+using Dapper;
 
 namespace SharpDevelopMVC4.Controllers
 {
@@ -10,6 +12,22 @@ namespace SharpDevelopMVC4.Controllers
     {
         public ActionResult Index()
         {
+        	string mdb = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + AppDomain.CurrentDomain.GetData("DataDirectory") + @"\MyAccessDb.mdb";
+        	var contact = new 
+        	{
+        		FullName = "Hewbhurt Gabon",
+        		Email = "gabs@gmail.com",
+        		BirthDate = "1981/09/17"
+        	};
+        	
+        	using (var conn = new OleDbConnection(mdb))
+        	{
+//        		conn.Execute( "INSERT INTO contacts(FullName, Email, BirthDate) "
+//	            	+ "VALUES (@FullName, @Email, @BirthDAte)", contact);
+        		var contactList = conn.Query("Select Id, FullName, Email, BirthDate from contacts").ToList();
+				ViewBag.Data = Newtonsoft.Json.JsonConvert.SerializeObject(contactList);
+        	}
+        	
             return View();
         }
 
