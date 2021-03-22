cfcb5cbad0fe84e207a8d28e31416e6cb422a969
Bernard Gabon <hewbertgabon@gmail.com>
2021-03-22 14:55:40 +0800

    contact song

---
+++ SharpDevelopMVC4/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SharpDevelopMVC4.Controllers
+{
+	
+	public class ContactController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+	
+}
---
+++ SharpDevelopMVC4/Controllers/SongController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SharpDevelopMVC4.Controllers
+{
+	public class SongController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		
+		public ActionResult GetSong()
+		{
+			return View();
+		}
+
+	}
+}
